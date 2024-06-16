e49e116cedcb9d3b535e2ab26c35282f05f16d75
huangxz <huangxingzhan@outlook.com>
2024-06-17 01:45:04 +0800

    log

--- android-log/src/main/java/vongshin/log/file/FileOutWriter.java
+++ android-log/src/main/java/vongshin/log/file/FileOutWriter.java
@@ -1,5 +1,40 @@
 package vongshin.log.file;
 
-final public class FileOutWriter {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+final public class FileOutWriter implements AutoCloseable {
+    private FileWriter writer;
+    public FileOutWriter(File outFile){
+        try {
+            writer = new FileWriter(outFile, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void write(String data){
+        try {
+            writer.write(data);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void writeln(String data){
+        try {
+            writer.write(data+ System.lineSeparator());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        writer.close();
+    }
 }
---
+++ android-log/src/main/java/vongshin/log/rolling/RollingPolicy.java
@@ -0,0 +1,4 @@
+package vongshin.log.rolling;
+
+public interface RollingPolicy {
+}
